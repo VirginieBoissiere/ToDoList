7edfbb29f1fc59c692e5bc2036410bb4d988d40e
Boissiere Virginie <99214081+VirginieBoissiere@users.noreply.github.com>
2022-08-30 17:19:16 +0200

    delete et archive ok

--- script.js
+++ script.js
@@ -11,6 +11,45 @@ console.log(listItems);
 listItems.innerHTML=localStorage.getItem('todo')
 
 
+// je selection et stock delete existants dans la page
+const btnDelete = document.querySelectorAll('.btn-delete'); 
+
+  // je fait une boucle pour agir de la même façon pour chaque élement de la liste pour delete
+  btnDelete.forEach(i=> {
+    console.log(btnDelete); 
+    // je place un espion pour ecouté le click du btn-delete
+    i.addEventListener('click',function(){
+        
+        console.log('bouton delete cliqué');
+        // j'enleve l'élément parent 
+        i.parentElement.remove();
+        localStorage.setItem("todo",listItems.innerHTML); 
+
+    })
+  })
+  // je selectionne et stock le btn-archive existants dans la page
+  const btnArchive =document.querySelectorAll('.btn-archive');
+  console.log(btnArchive);
+   // je fait une boucle pour agir de la même façon pour chaque élement de la liste pour archive
+   btnArchive.forEach (i=>{
+    console.log(btnArchive);
+     // je place un espion pour ecouté le click du btn-archive
+     i.addEventListener('click',function(){
+    
+        console.log('bouton archive cliqué');
+        // je selectionne l'element parent de i
+        const parent =i.parentElement;
+        // j'agis sur la div parent 
+        parent.classList.toggle('done');
+
+      
+    })
+
+
+
+
+  })
+
 // je place un espion sur le submint 
 form.addEventListener ('submit',function (e) {
     // j'empêche la page de se rafraichir
@@ -37,40 +76,12 @@ form.addEventListener ('submit',function (e) {
     localStorage.setItem("todo",listItems.innerHTML); 
 
    
-    // je selection et stock delete existants dans la page
-    const btnDelete = document.querySelectorAll('.btn-delete'); 
-   // je selectionne et stock le btn-archive existants dans la page
-   const btnArchive =document.querySelectorAll('.btn-archive');
-   console.log(btnArchive);
-
-
-   // je fait une boucle pour agir de la même façon pour chaque élement de la liste pour delete
-    btnDelete.forEach(i=> {
-        console.log(btnDelete); 
-        // je place un espion pour ecouté le click du btn-delete
-        i.addEventListener('click',function(){
-            console.log('bouton delete cliqué');
-            // j'enleve l'élément parent 
-            i.parentElement.remove();
-
-        })
-    })
-    // je fait une boucle pour agir de la même façon pour chaque élement de la liste pour archive
-    btnArchive.forEach (i=>{
-        console.log(btnArchive);
-         // je place un espion pour ecouté le click du btn-archive
-         i.addEventListener('click',function(){
-            console.log('bouton archive cliqué');
-            // je selectionne l'element parent de i
-            const parent =i.parentElement;
-            // j'agis sur la div parent 
-            parent.classList.toggle('done');
    
-          
-        })
         
+    // reinitialiser le formulaire 
+    form.reset();
+      // On rafraichit la page pour faire apparaitre les informations
+   window.location.href = window.location.href;
     })
-// reinitialiser le formulaire 
-form.reset();
-});
+
 
